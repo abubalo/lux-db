824756a41ceaa10a96fd100a6cafd560e2942caa
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-30 20:02:34 +0200

    Implement 'where' query API

---
+++ src/util/where.ts
@@ -0,0 +1,44 @@
+import { Collector, Comparator, KeyChain } from "../types/types";
+
+export const where = <T, R>(collector: Collector<T>, runner: Promise<R>) => {
+  return (key: KeyChain<T>) => {
+    return {
+      equals(val: unknown) {
+        collector(key, Comparator.Equals, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      notEquals(val: unknown) {
+        collector(key, Comparator.NotEqual, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      greatherThan(val: unknown) {
+        collector(key, Comparator.GreaterThan, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      lessThan(val: unknown) {
+        collector(key, Comparator.LessThan, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      gretherOrEqual(val: unknown) {
+        collector(key, Comparator.GreaterOrEqual, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      lessThanOrEqual(val: unknown) {
+        collector(key, Comparator.LessThanOrEqual, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      in(val: Array<unknown>) {
+        collector(key, Comparator.In, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      between(val: [number, number]) {
+        collector(key, Comparator.Between, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+      matches(val: string | RegExp) {
+        collector(key, Comparator.Matches, val);
+        return { where: where<T, R>(collector, runner), run: runner };
+      },
+    };
+  };
+};
