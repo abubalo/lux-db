2f62730cc585e71ab654beed00b73b54f813f2a6
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-30 21:51:50 +0200

    create index.ts for interactive

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { JSONDB } from "./JSONDB";
+
+interface Todo {
+  id: number;
+  name: string;
+  status: "pending" | "completed" | "archive";
+  author: {
+    name: string;
+  };
+}
+
+const db = new JSONDB<Todo>("todo");
+
+(async () => {
+    const todo = db.insert({
+      id: Math.floor(Math.random() * 10_000_000),
+      name: "Mop the floor",
+      status: "pending",
+      author: {
+        name: "Jamiu",
+      },
+    });
+
+  const todo = db
+    .select("name", "author.name", "status")
+    .where("status")
+    .equals("pending")
+    .where("author.name")
+    .equals("John Doe")
+    .run();
+
+  console.log(todo);
+})();
