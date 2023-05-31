2f306a7127c8cd91fe00a7cecfaee82c46a1d27b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-31 22:49:27 +0200

    extract where logic into collect file

---
+++ src/util/collect.ts
@@ -0,0 +1,15 @@
+import { Comparator, Matcher } from "../types/types"
+import { where } from "./where";
+import { KeyChain } from "../types/types";
+
+export const collect = <T, R> (done: (res: Array<Matcher<T>>) => R)=>{
+    const matchers: Array<Matcher <T>> = [];
+
+    const run = async () => done(matchers);
+
+    return {
+        where: where<T, R>(collector: (key: KeyChain<T>, comparator : Comparator, value: unknown) =>{
+            matchers.push({key, comparator, value})
+        }, run), run
+    }
+}
