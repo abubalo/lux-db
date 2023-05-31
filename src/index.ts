cc2eb2664fc884833c0bfe28e752908d2a70a792
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-31 22:46:48 +0200

    made a minor edit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { JSONDB } from "./JSONDB";
+import { JSONDB } from "./JsonAPI";
 
 interface Todo {
   id: number;
@@ -12,14 +12,14 @@ interface Todo {
 const db = new JSONDB<Todo>("todo");
 
 (async () => {
-    const todo = db.insert({
-      id: Math.floor(Math.random() * 10_000_000),
-      name: "Mop the floor",
-      status: "pending",
-      author: {
-        name: "Jamiu",
-      },
-    });
+    // const todo = db.insert({
+    //   id: Math.floor(Math.random() * 10_000_000),
+    //   name: "Mop the floor",
+    //   status: "pending",
+    //   author: {
+    //     name: "Jamiu",
+    //   },
+    // });
 
   const todo = db
     .select("name", "author.name", "status")
