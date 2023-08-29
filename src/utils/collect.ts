851712a3fb7b75e6b7085469ac3cd0346e7dbfb0
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-29 10:46:53 +0200

    Rename util directory to utils

--- src/utils/collect.ts
+++ src/utils/collect.ts
@@ -8,9 +8,9 @@ export const collect = <T, R> (done: (res: Array<Matcher<T>>) => R)=>{
     const run = async () => done(matchers);
 
     return {
-        where: where<T, R>(collector: (key: KeyChain<T>, comparator : Comparator, value: unknown) =>{
+        where: where<T, R>((key: KeyChain<T>, comparator : Comparator, value: unknown) =>{
             matchers.push({key, comparator, value})
-        }, run), 
+        }, run()), 
         run
     }
 }
