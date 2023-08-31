6a4b768c12add46440265364e94fcd7ff1b8bf4d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-01 00:00:27 +0200

    docs: Add detailed code documentation for class methods
    Added detailed comments to each method of the JSONDatabase class to provide clear explanations of their functionality, accepted parameters, and expected return values. This documentation enhances code readability and helps future developers understand and use the class effectively.

--- src/JsonAPI.ts
+++ src/JsonAPI.ts
@@ -1,36 +1,62 @@
 import * as path from "path";
 import fs from "fs";
 import { readFile, writeFile } from "fs/promises";
-import { where } from "./utils/where";
 import { KeyChain, Matcher, ObjectLiteral } from "../types";
 import { collect } from "./utils/collect";
 import { matchDataKayValue } from "./utils/match-data-key-value";
 import { createItemFromKeys } from "./utils/create-items-from-keys";
 
-export class JSONDatatbase<T extends object> {
+/**
+ * Represents a simple JSON database with query capabilities.
+ * @template T - Type of the database items.
+ */
+export default class JSONDatatbase<T extends object> {
   private readonly filePath: string;
   private _size = 0;
 
+  /**
+   * The number of items in the database.
+   */
   get size() {
     return this._size;
   }
 
+  /**
+   * Creates a new JSONDatabase instance.
+   * @param fileName - The name of the database file (without extension).
+   */
   constructor(private fileName: string) {
+    // Construct the file path
     this.filePath = path.join(__dirname + "/db", `${fileName}.json`);
-
-    if (!fs.existsSync(this.filePath)) {
-      fs.writeFileSync(this.filePath, JSON.stringify([]), {
-        encoding: "utf-8",
-      });
+    try {
+      // Create the database file if it doesn't exist
+      if (!fs.existsSync(this.filePath)) {
+        fs.writeFileSync(this.filePath, JSON.stringify([]), {
+          encoding: "utf-8",
+        });
+      }
+    } catch (error) {
+      console.error(`Error reading file: ${this.filePath}`, error);
+      throw new Error(`Failed to read data from ${this.fileName}`);
     }
   }
 
+  /**
+   * Insert new data into the database.
+   * @param item - The item to be inserted.
+   * @returns Promise resolving to the inserted item.
+   */
   public async insert(item: T): Promise<T> {
     await this.save(item);
     this._size += 1;
     return item;
   }
 
+  /**
+   * Retrieve a single item from the database based on specified keys.
+   * @param keys - Keys to match for retrieving the item.
+   * @returns Promise resolving to the retrieved item, or null if not found.
+   */
   public getOne(...keys: KeyChain<T>[]) {
     return collect<T, Promise<T | Partial<T> | null>>(
       async (matchers: Matcher<T>[]) => {
@@ -51,6 +77,12 @@ export class JSONDatatbase<T extends object> {
       }
     );
   }
+
+  /**
+   * Retrieve multiple items from the database based on specified keys.
+   * @param keys - Keys to match for retrieving items.
+   * @returns Promise resolving to an array of retrieved items, or an array of partial items if keys are provided.
+   */
   public getAll(...keys: KeyChain<T>[]) {
     return collect<T, Promise<T[] | Partial<T>[]>>(
       async (matchers: Matcher<T>[]) => {
@@ -72,6 +104,11 @@ export class JSONDatatbase<T extends object> {
     );
   }
 
+  /**
+   * Update a single item in the database based on specified matchers (keys).
+   * @param data - Partial data with fields to update in the matching item.
+   * @returns Promise resolving to the updated item, or null if no match found.
+   */
   public updateOne(data: Partial<T>) {
     return collect<T, Promise<T | null>>(async (matchers: Matcher<T>[]) => {
       const list = await this.read();
@@ -88,6 +125,11 @@ export class JSONDatatbase<T extends object> {
     });
   }
 
+  /**
+   * Update multiple items in the database based on specified matchers (keys).
+   * @param data - Partial data with fields to update in the matching items.
+   * @returns Promise resolving to an array of updated items.
+   */
   public updateAll(data: Partial<T>) {
     return collect<T, Promise<T[]>>(async (matchers: Matcher<T>[]) => {
       const updateItems: T[] = [];
@@ -106,6 +148,10 @@ export class JSONDatatbase<T extends object> {
     });
   }
 
+  /**
+   * Delete a single item from the database based on specified matchers (keys).
+   * @returns Promise resolving to the deleted item, or null if no match found.
+   */
   public deleteOne() {
     return collect<T, Promise<T | null>>(async (matchers: Matcher<T>[]) => {
       const list = await this.read();
@@ -122,6 +168,10 @@ export class JSONDatatbase<T extends object> {
     });
   }
 
+  /**
+   * Delete multiple items from the database based on specified matchers (keys).
+   * @returns Promise resolving to an array of deleted items.
+   */
   public deleteAll() {
     return collect<T, Promise<T[]>>(async (matchers: Matcher<T>[]) => {
       const deleteItems: T[] = [];
@@ -142,10 +192,20 @@ export class JSONDatatbase<T extends object> {
     });
   }
 
+  /**
+   * Read the database file and parse its content.
+   * @returns Promise resolving to an array of database items.
+   * @private
+   */
   private async read(): Promise<Array<T>> {
     return JSON.parse(await readFile(this.filePath, "utf-8"));
   }
 
+  /**
+   * Save data to the database file.
+   * @param data - Data to be saved (single item or array of items).
+   * @private
+   */
   private async save(data: T | Array<T>) {
     try {
       let content = data;
@@ -153,9 +213,9 @@ export class JSONDatatbase<T extends object> {
         content = await this.read();
         content.push(data);
       }
-      return writeFile(this.filePath, JSON.stringify(content));
+      await writeFile(this.filePath, JSON.stringify(content));
     } catch (error) {
-      console.log(error);
+      console.error(`Error saving data to file: ${this.filePath}`, error);
       throw new Error(`Failed to save data to ${this.fileName}`);
     }
   }
