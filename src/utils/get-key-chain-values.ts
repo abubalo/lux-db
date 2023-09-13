0eeb2230f68a9048cea5741c83888e9dce4bb416
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-13 20:24:29 +0200

    Add code documentation for the 'getKeyChainValue' function

--- src/utils/get-key-chain-values.ts
+++ src/utils/get-key-chain-values.ts
@@ -1,5 +1,13 @@
-import { ObjectLiteral } from "../../types";
+import { ObjectLiteral } from "../../types/index";
 
+/**
+ * Get the value associated with a given key chain from a source object.
+ *
+ * @param {string} keyChain - The dot-separated key chain to traverse within the source object.
+ * @param {ObjectLiteral} data - The source object from which to retrieve the value.
+ * @returns {unknown} - The value associated with the specified key chain.
+ * @throws {Error} If the key chain is invalid or if a key in the chain does not exist in the source object.
+ */
 export const getKeyChainValue = (
   keyChain: string,
   data: ObjectLiteral
@@ -8,12 +16,13 @@ export const getKeyChainValue = (
   const key = parts.shift() as string;
   const value = data[key] as ObjectLiteral;
 
-  if(parts.length){
-    if(value && typeof value === "object"){
-        return getKeyChainValue(parts.join(), value);
+  if (parts.length) {
+    if (value && typeof value === "object") {
+      return getKeyChainValue(parts.join("."), value);
     }
-    
-    throw new Error(`Cannot get "${parts.join()}" of ${value}`)
+
+    throw new Error(`Cannot get "${parts.join(".")}" of ${value}`);
   }
 
+  return value;
 };
