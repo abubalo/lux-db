c6d00cc751c8d9c5eff67d76e3521b1bc26f8495
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-10-11 11:08:04 +0200

    chore: Modify jest config and change extension to .ts

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type {Config} from "@jest/types"
+
+const config: Config.InitialOptions = {
+  "preset": "ts-jest",
+  transform: {
+    '^.+\\.ts?$': ['ts-jest', {
+      tsconfig: './tsconfig.json',
+    }],
+  },
+  testEnvironment: 'node', // Set the test environment to Node.js
+  transformIgnorePatterns: [
+    '/node_modules/', // Ignore all code in node_modules by default
+  ],
+  testRegex: './src/.*\\.(test|spec)?\\.(ts)$', // Match TypeScript test files
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  roots: ['<rootDir>src'],
+  verbose: true,
+  automock: true,
+};
+
+
+export default config
