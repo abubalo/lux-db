1fd7999f30dbc76ac3de7abb22fd9945a96ff1aa
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2024-01-23 23:25:47 +0200

    refactor: Simplified the LuxDB singleton pattern for better clarity and TypeScript adherence

--- src/lux-db.ts
+++ src/lux-db.ts
@@ -1,24 +1,12 @@
-import {LuxDB} from './lib/LuxDB';
+import { LuxDB } from './lib/LuxDB';
 import crypto from 'crypto';
 
-// Singleton instance
-let instance: LuxDB<any> | null = null;
+function luxdb<T extends object>(filename: string, destination?: string) {
+  return LuxDB.creatInstance<T>(filename, destination);
+}
 
-/**
- * Create and return a single/global instance of the LuxDB.
- * @param fileName - The name of the database file (without extension).
- * @param destination - The folder where the databases will be located.
- * @returns The LuxDB instance.
- */
-const luxdb = <T extends object>(fileName: string, destination?: string): LuxDB<T> => {
-  if (!instance) {
-    instance = new LuxDB<T>(fileName, destination);
-  }
-  return instance as LuxDB<T>;
+const autoId = (length = 10): string => {
+  return crypto.randomBytes(length).toString('hex');
 };
 
-const autoId = (): string => {
-  return crypto.randomBytes(10).toString('hex');
-};
-
-export { luxdb as default, autoId };
+export { luxdb, autoId };
